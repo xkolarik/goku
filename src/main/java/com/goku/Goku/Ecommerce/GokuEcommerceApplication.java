ca48be81d57737f487c2f9461f65daea4552a28f
plisboa <paulo.lisboa@ext.hdi.com.br>
2020-11-18 20:22:58 -0300

    Criação do repositorio e estrutura do projeto goku

---
+++ src/main/java/com/goku/Goku/Ecommerce/GokuEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.goku.Goku.Ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GokuEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GokuEcommerceApplication.class, args);
+	}
+
+}
