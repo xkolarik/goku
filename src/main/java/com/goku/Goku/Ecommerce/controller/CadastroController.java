da7ea69b7c4a3b825f76d1582ca478e0fa4a1cda
plisboa <paulo.lisboa@ext.hdi.com.br>
2020-11-18 20:42:33 -0300

    Criação estrutura do projeto

---
+++ src/main/java/com/goku/Goku/Ecommerce/controller/CadastroController.java
@@ -0,0 +1,4 @@
+package com.goku.Goku.Ecommerce.controller;
+
+public class CadastroController {
+}
---
+++ src/main/java/com/goku/Goku/Ecommerce/controller/advice/Advice.java
@@ -0,0 +1,12 @@
+package com.goku.Goku.Ecommerce.controller.advice;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class Advice {
+
+    private static final Logger log = LoggerFactory.getLogger(Advice.class);
+
+}
